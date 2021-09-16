d18d170fd3fb286076a94eba4df1a5604fff80e2
ikz <75650546+communication-engineer-ikz@users.noreply.github.com>
2021-09-17 00:49:44 +0900

    null、空文字判定をStringUtils 使ってまとめた

--- src/readable/code/playground/Strings.java
+++ src/readable/code/playground/Strings.java
@@ -1,16 +1,16 @@
 package readable.code.playground;
 
+import org.apache.commons.lang3.StringUtils;
+
 public final class Strings {
 
 	// TODO1: refactor this method after you read Readable Code 7.5
 	public static String capitalize(String str) {
 
-		if (str == null) {
+		if (StringUtils.isEmpty(str)) {
 			return str;
 		};
-		if (str.length() == 0) {
-			return str;
-		};
+
 		if (str.length() == 1) {
 			return str.toUpperCase();
 		};
